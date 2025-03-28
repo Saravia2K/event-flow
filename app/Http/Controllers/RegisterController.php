a2d4ebc40b69ce500ea9fe722e59b829f7297faf
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-03-28 10:41:35 -0600

    Actualizando redirectPath

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -43,7 +43,7 @@ class RegisterController extends Controller
     {
         $user = User::find(Auth::id());
         if ($user->isAdmin()) {
-            return route("organizer-dashboard");
+            return route("organizer.dashboard");
         }
         return route("index");
     }
